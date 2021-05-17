a9d71e3d60db1e50a51c975a8e62c0d15f8904e6
Gustavo Gomes <gustavo@spigo.net>
2021-05-17 20:08:42 -0300

    Classe jogador

---
+++ Assets/Mortal_Clicker/ODA/Jogador.cs
@@ -0,0 +1,11 @@
+﻿[System.Serializable]
+public struct Jogador
+{
+    [System.Serializable]
+    public struct LinhaJogador
+    {
+        public string login;
+        public string senha;
+    }
+    public LinhaJogador[] objetos;
+}
